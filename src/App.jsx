0abb3564e6ae57c63440525db4664bed8853c6a7
Gorka Reguero <reguerogorka@gmail.com>
2025-04-15 13:11:29 +0200

    cambios para probar app desplegada

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,16 @@ function App() {
   const [chaptersES, setChaptersES] = useState([]);
   const [chaptersEN, setChaptersEN] = useState([]);
  
-
+  const BASE_URL  = 'https://backmangadownloader.onrender.com/'
 
   const handleSearch = async () => {
     try {
-      const res1 = await fetch(`http://localhost:3001/api/manga/search?title=${title}`);
+      const res1 = await fetch(`${BASE_URL}/api/manga/search?title=${title}`);
       const manga = await res1.json();
 
       if (!manga?.id) return alert('Manga no encontrado');
 
-      const res2 = await fetch(`http://localhost:3001/api/manga/${manga.id}/chapters?start=${start}&end=${end}`);
+      const res2 = await fetch(`${BASE_URL}/api/manga/${manga.id}/chapters?start=${start}&end=${end}`);
       const chapterData = await res2.json();
 
       
@@ -45,7 +45,7 @@ setChaptersEN(uniqueENChapters);
 
   const handleDownloadChapter = async (chapterId, lang) => {
     try {
-      const response = await fetch(`http://localhost:3001/api/manga/${chapterId}/download?lang=${lang}`);     
+      const response = await fetch(`${BASE_URL}/api/manga/${chapterId}/download?lang=${lang}`);     
       const blob = await response.blob();
       const url = window.URL.createObjectURL(blob);
   
@@ -62,7 +62,7 @@ setChaptersEN(uniqueENChapters);
   const handleDownloadAll = async (lang) => {
     try {
       chaptersES.forEach(async (chap) => {
-        const response = await fetch(`http://localhost:3001/api/manga/${chap.id}/downloadAll?lang=${lang}&start=${start}&end=${end}`);
+        const response = await fetch(`${BASE_URL}/api/manga/${chap.id}/downloadAll?lang=${lang}&start=${start}&end=${end}`);
         const blob = await response.blob();
         const url = window.URL.createObjectURL(blob);
     
